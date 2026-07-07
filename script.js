f985f9e66dd50dcbc807bedeaa1cdca62b5be839
Fco Gabriel <fco.gabrielsilva2408@gmail.com>
2026-07-07 20:40:54 -0300

    fix: ajusta caminho dos assets

--- script.js
+++ script.js
@@ -11,10 +11,10 @@ const iniciarOuPausarBt = document.querySelector("#start-pause span");
 const iniciarOuPausarImg = document.querySelector("#start-pause img");
 const tempoNaTela = document.querySelector("#timer");
 
-const musica = new Audio("/sons/luna-rise-part-one.mp3");
-const audioPlay = new Audio("/sons/play.wav");
-const audioPausa = new Audio("/sons/pause.mp3");
-const audioTempoFinalizado = new Audio("/sons/beep.mp3");
+const musica = new Audio("./sons/luna-rise-part-one.mp3");
+const audioPlay = new Audio("./sons/play.wav");
+const audioPausa = new Audio("./sons/pause.mp3");
+const audioTempoFinalizado = new Audio("./sons/beep.mp3");
 
 let tempoDecorridoEmSegundos = 1500;
 let intervaloId = null;
@@ -53,7 +53,7 @@ function alterarContexto(contexto) {
     contexto.classList.remove("active");
   });
   html.setAttribute("data-contexto", contexto);
-  banner.setAttribute("src", `/imagens/${contexto}.png`);
+  banner.setAttribute("src", `./imagens/${contexto}.png`);
   switch (contexto) {
     case "foco":
       titulo.innerHTML = `
@@ -98,13 +98,13 @@ function iniciarOuPausar() {
   audioPlay.play();
   intervaloId = setInterval(contagemRegressiva, 1000);
   iniciarOuPausarBt.textContent = "Pausar";
-  iniciarOuPausarImg.setAttribute("src", "/imagens/pause.png");
+  iniciarOuPausarImg.setAttribute("src", "./imagens/pause.png");
 }
 
 function zerar() {
   clearInterval(intervaloId);
   iniciarOuPausarBt.textContent = "Começar";
-  iniciarOuPausarImg.setAttribute("src", "/imagens/play_arrow.png");
+  iniciarOuPausarImg.setAttribute("src", "./imagens/play_arrow.png");
   intervaloId = null;
 }
 
