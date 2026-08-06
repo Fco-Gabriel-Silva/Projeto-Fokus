2e0cd617429379bd581d153c00bfe0d4c003bfd0
Fco Gabriel <fco.gabrielsilva2408@gmail.com>
2026-08-06 16:12:48 -0300

    Selecionando uma tarefa

--- script-crud.js
+++ script-crud.js
@@ -5,6 +5,9 @@ const btnCancelarTarefa = document.querySelector(
 const formAdicionarTarefa = document.querySelector(".app__form-add-task");
 const textarea = document.querySelector(".app__form-textarea");
 const ulTarefas = document.querySelector(".app__section-task-list");
+const paragrafoDescricaoTarefa = document.querySelector(
+  ".app__section-active-task-description",
+);
 
 const tarefas = JSON.parse(localStorage.getItem("tarefas")) || [];
 
@@ -37,9 +40,9 @@ function criarElementoTarefa(tarefa) {
   botao.classList.add("app_button-edit");
 
   botao.onclick = () => {
-    debugger;
+    // debugger;
     const novaDescricao = prompt("Qual é o novo nome da tarefa?");
-    console.log("Nova descrição da tarefa: ", novaDescricao);
+    // console.log("Nova descrição da tarefa: ", novaDescricao);
     if (novaDescricao) {
       paragrafo.textContent = novaDescricao;
       tarefa.descricao = novaDescricao;
@@ -55,6 +58,11 @@ function criarElementoTarefa(tarefa) {
   li.append(svg);
   li.append(paragrafo);
   li.append(botao);
+
+  li.onclick = () => {
+    paragrafoDescricaoTarefa.textContent = tarefa.descricao;
+    li.classList.add("app__section-task-list-item-active");
+  };
 
   return li;
 }
