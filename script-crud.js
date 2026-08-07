62c858f4b2a148f088e9ebc3b7de656f2bcf34c8
Fco Gabriel <fco.gabrielsilva2408@gmail.com>
2026-08-07 18:37:37 -0300

    Finalizando uma tarefa

--- script-crud.js
+++ script-crud.js
@@ -11,6 +11,7 @@ const paragrafoDescricaoTarefa = document.querySelector(
 
 const tarefas = JSON.parse(localStorage.getItem("tarefas")) || [];
 let tarefaSelecionada = null;
+let liTarefaSelecionada = null;
 
 function atualizarTarefas() {
   localStorage.setItem("tarefas", JSON.stringify(tarefas));
@@ -70,10 +71,12 @@ function criarElementoTarefa(tarefa) {
     if (tarefaSelecionada == tarefa) {
       paragrafoDescricaoTarefa.textContent = "";
       tarefaSelecionada = null;
+      liTarefaSelecionada = null;
       return;
     }
 
     tarefaSelecionada = tarefa;
+    liTarefaSelecionada = li;
     paragrafoDescricaoTarefa.textContent = tarefa.descricao;
     li.classList.add("app__section-task-list-item-active");
   };
@@ -103,4 +106,14 @@ formAdicionarTarefa.addEventListener("submit", (evento) => {
 tarefas.forEach((tarefa) => {
   const elementoTarefa = criarElementoTarefa(tarefa);
   ulTarefas.append(elementoTarefa);
+});
+
+document.addEventListener("FocoFinalizado", () => {
+  if (tarefaSelecionada && liTarefaSelecionada) {
+    liTarefaSelecionada.classList.remove("app__section-task-list-item-active");
+    liTarefaSelecionada.classList.add("app__section-task-list-item-complete");
+    liTarefaSelecionada
+      .querySelector("button")
+      .setAttribute("disabled", "disabled");
+  }
 });
